2d11c80a0cd7831f11b9c31ad50d37ef48125519
Johannes Bruvik <jbb@netpower.no>
2022-05-03 15:51:54 -0400

    iOS stamp style

--- packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapPolylineController.m
+++ packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapPolylineController.m
@@ -92,7 +92,7 @@ static void InterpretPolylineOptions(NSDictionary *data, id<FLTGoogleMapPolyline
     NSArray *icon = data[@"iosStampStyle"];
     if (icon) {
         UIImage *image = ExtractIcon(registrar, icon);
-        [sink setIcon:image];
+        [sink setStampStyle:image];
     }
 
   NSNumber *zIndex = data[@"zIndex"];
